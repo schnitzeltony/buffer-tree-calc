6565fa534aeb0cdb121acc95bfda196eaace6c51
Andreas Müller <schnitzeltony@gmail.com>
2022-02-25 12:29:51 +0100

    More pseudo code
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- buffercalcbrainstorming.h
+++ buffercalcbrainstorming.h
@@ -4,29 +4,90 @@
 #include <memory>
 #include <vector>
 
+class AbstractAccessStrategy
+{
+public:
+    typedef std::shared_ptr<AbstractAccessStrategy> Ptr;
+    virtual bool tryMakeBusy() = 0;
+    virtual void setDone(bool done) = 0;
+    virtual bool isDone() = 0;
+    virtual void reset() = 0;
+};
+
+class SingleThreadedBusyController : public AbstractAccessStrategy
+{
+public:
+    virtual bool tryMakeBusy() override
+    {
+        if(m_busy) {
+            return false;
+        }
+        m_busy = true;
+        return true;
+    }
+    virtual void setDone(bool done) override
+    {
+        m_done = done;
+    }
+    virtual bool isDone() override
+    {
+        return m_done;
+    }
+    virtual void reset() override
+    {
+        m_busy = false;
+        m_done = false;
+    }
+
+protected:
+    bool m_busy = false;
+    bool m_done = false;
+};
+
+
 class AbstractCalculationComponent
 {
 public:
-    typedef std::shared_ptr<AbstractCalculationComponent> Ptr;
+    AbstractCalculationComponent(AbstractAccessStrategy::Ptr busyState);
+
+    virtual void init() {}
     virtual void prepareCalc() {}
-    virtual void startCalc(int subBufferNo) = 0;
-    virtual bool calcDone() = 0;
+    bool startCalc(int subBufferNo)
+    {
+        bool calcDoneByMe = m_busyState->tryMakeBusy();
+        if(calcDoneByMe) {
+            doCalc(subBufferNo);
+            m_busyState->setDone(true);
+        }
+        return calcDoneByMe;
+    }
+    bool calcDone()
+    {
+        return m_busyState->isDone();
+    }
+    void setDone(bool done) {
+        m_busyState->setDone(done);
+    }
+    virtual void destroy() {}
+protected:
+    virtual bool doCalc(int subBufferNo) = 0;
+
+    AbstractAccessStrategy::Ptr m_busyState;
 };
+
+typedef std::shared_ptr<AbstractCalculationComponent> AbstractCalculationComponentPtr;
+
 
 
 class AbstractCalculationBuffer: public AbstractCalculationComponent
 {
 public:
-    AbstractCalculationBuffer(int size);
+    AbstractCalculationBuffer(AbstractAccessStrategy::Ptr busyStrategy,int size);
     int getSize();
-    virtual void prepareCalc() override  { m_calcDone = false; }
-    virtual void startCalc(int subBufferNo = 0) override { (void)subBufferNo; m_calcDone = true; }
-    virtual bool calcDone() override { return m_calcDone; }
 private:
-    bool m_calcDone = false;
 };
 
-class ConreteBufferFloatBare: public AbstractCalculationBuffer
+class ConreteBufferFloatBareSingle: public AbstractCalculationBuffer
 {
 public:
     float *getBuffer();
@@ -35,36 +96,66 @@ public:
 class ConreteBufferFloatVector: public AbstractCalculationBuffer
 {
 public:
-    std::vector<float> getBuffer();
+    std::vector<float> &getBuffer();
 };
 
 
-class AbstractCalculationStrategy
+class AbstractCalculationStrategy : public AbstractCalculationComponent
 {
 public:
-    typedef std::shared_ptr<AbstractCalculationStrategy> Ptr;
-    virtual void doCalc() = 0;
+    AbstractCalculationStrategy(std::vector<AbstractCalculationComponentPtr> inputComponents,
+                                AbstractCalculationComponentPtr outComponent);
+
+    virtual std::vector<AbstractCalculationComponentPtr> getInputComponents();
+    virtual AbstractCalculationComponentPtr getOutputBuffer();
+
+private:
+    std::vector<AbstractCalculationComponentPtr> m_inputComponents;
+    AbstractCalculationComponentPtr m_outComponent;
 };
+
+typedef std::shared_ptr<AbstractCalculationStrategy> AbstractCalculationStrategyPtr;
+
 
 class CalculationNode: public AbstractCalculationComponent
 {
 public:
-    virtual std::vector<AbstractCalculationComponent::Ptr> getInputComponents() = 0;
-    virtual AbstractCalculationComponent::Ptr getOutputBuffer();
-    virtual void startCalc(int subBufferNo) override
+    virtual bool doCalc(int subBufferNo) override
     {
-        for(auto input : getInputComponents()) {
+        bool allWorkDone = true;
+        for(auto input : m_outCalcStrategy->getInputComponents()) {
             if(!input->calcDone()) {
-                input->startCalc(subBufferNo);
+                bool started = input->startCalc(subBufferNo);
+                if(!started) {
+                    allWorkDone = false;
+                }
             }
         }
+        if(allWorkDone && !m_outCalcStrategy->calcDone()) {
+            bool started = m_outCalcStrategy->startCalc(subBufferNo);
+            if(!started) {
+                allWorkDone = false;
+            }
+        }
+        if(allWorkDone) {
+            m_outCalcStrategy->setDone(true);
+        }
+        return allWorkDone;
     }
-    virtual bool calcDone() override
+    virtual void init() override
     {
-        return getOutputBuffer()->calcDone();
+        for(auto input : m_outCalcStrategy->getInputComponents()) {
+            input->init();
+        }
+        m_outCalcStrategy->getOutputBuffer()->init();
     }
+    virtual void destroy() override
+    {
+
+    }
+
 private:
-    AbstractCalculationStrategy::Ptr m_outCalcStrategy;
+    AbstractCalculationStrategyPtr m_outCalcStrategy;
 };
 
 
