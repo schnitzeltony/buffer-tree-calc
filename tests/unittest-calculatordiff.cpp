240ed52fec9794ee89ac4a379aa329917df9abc2
Andreas Müller <schnitzeltony@gmail.com>
2022-03-07 15:28:32 +0100

    Test & fix CalculatorDiff
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/unittest-calculatordiff.cpp
+++ tests/unittest-calculatordiff.cpp
@@ -9,7 +9,7 @@ TEST(TEST_DIFF_CALC, SUBSTRACT_VALID_INT16) {
     CALC_PTR(int16_t) calcMinuent = CalculatorNull<int16_t>::createWithOutBuffer<SingleThreadedAccessStrategy>(10);
     BUFFER_PTR(int16_t) buffMinuent = calcMinuent->getOutputBuffer();
     CALC_PTR(int16_t) calcSubtrahent = CalculatorNull<int16_t>::createWithOutBuffer<SingleThreadedAccessStrategy>(10);
-    BUFFER_PTR(int16_t) buffSubtrahent = calcMinuent->getOutputBuffer();
+    BUFFER_PTR(int16_t) buffSubtrahent = calcSubtrahent->getOutputBuffer();
 
     CALC_PTR(int16_t) calcDiff = CalculatorDiff<int16_t>::createWithOutBuffer<SingleThreadedAccessStrategy>(
                 calcMinuent,
@@ -20,5 +20,53 @@ TEST(TEST_DIFF_CALC, SUBSTRACT_VALID_INT16) {
     buffMinuent->at(0) = 5;
     buffSubtrahent->at(1) = 8;
     buffMinuent->at(1) = 4;
+    calcDiff->tryStartCalc(2);
 
+    BUFFER_PTR(int16_t) diffBuff = calcDiff->getOutputBuffer();
+    EXPECT_EQ(buffMinuent->at(0) - buffSubtrahent->at(0), diffBuff->at(0));
+    EXPECT_EQ(buffMinuent->at(1) - buffSubtrahent->at(1), diffBuff->at(1));
+}
+
+TEST(TEST_DIFF_CALC, SUBSTRACT_VALID_FLOAT) {
+    CALC_PTR(float) calcMinuent = CalculatorNull<float>::createWithOutBuffer<SingleThreadedAccessStrategy>(10);
+    BUFFER_PTR(float) buffMinuent = calcMinuent->getOutputBuffer();
+    CALC_PTR(float) calcSubtrahent = CalculatorNull<float>::createWithOutBuffer<SingleThreadedAccessStrategy>(10);
+    BUFFER_PTR(float) buffSubtrahent = calcSubtrahent->getOutputBuffer();
+
+    CALC_PTR(float) calcDiff = CalculatorDiff<float>::createWithOutBuffer<SingleThreadedAccessStrategy>(
+                calcMinuent,
+                calcSubtrahent,
+                10);
+
+    buffSubtrahent->at(0) = 10;
+    buffMinuent->at(0) = 5;
+    buffSubtrahent->at(1) = 8;
+    buffMinuent->at(1) = 4;
+    calcDiff->tryStartCalc(2);
+
+    BUFFER_PTR(float) diffBuff = calcDiff->getOutputBuffer();
+    EXPECT_FLOAT_EQ(buffMinuent->at(0) - buffSubtrahent->at(0), diffBuff->at(0));
+    EXPECT_FLOAT_EQ(buffMinuent->at(1) - buffSubtrahent->at(1), diffBuff->at(1));
+}
+
+TEST(TEST_DIFF_CALC, SUBSTRACT_VALID_DOUBLE) {
+    CALC_PTR(double) calcMinuent = CalculatorNull<double>::createWithOutBuffer<SingleThreadedAccessStrategy>(10);
+    BUFFER_PTR(double) buffMinuent = calcMinuent->getOutputBuffer();
+    CALC_PTR(double) calcSubtrahent = CalculatorNull<double>::createWithOutBuffer<SingleThreadedAccessStrategy>(10);
+    BUFFER_PTR(double) buffSubtrahent = calcSubtrahent->getOutputBuffer();
+
+    CALC_PTR(double) calcDiff = CalculatorDiff<double>::createWithOutBuffer<SingleThreadedAccessStrategy>(
+                calcMinuent,
+                calcSubtrahent,
+                10);
+
+    buffSubtrahent->at(0) = 10;
+    buffMinuent->at(0) = 5;
+    buffSubtrahent->at(1) = 8;
+    buffMinuent->at(1) = 4;
+    calcDiff->tryStartCalc(2);
+
+    BUFFER_PTR(double) diffBuff = calcDiff->getOutputBuffer();
+    EXPECT_DOUBLE_EQ(buffMinuent->at(0) - buffSubtrahent->at(0), diffBuff->at(0));
+    EXPECT_DOUBLE_EQ(buffMinuent->at(1) - buffSubtrahent->at(1), diffBuff->at(1));
 }
