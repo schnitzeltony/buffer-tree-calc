d8e8f447d47ec86bd5799da8344925f8f77287e8
Andreas Müller <schnitzeltony@gmail.com>
2022-03-07 16:05:31 +0100

    CalculatorDiff: Add scalar (one subtrahend for all) subtraction
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- calc-components/calculators/calculatordiff_impl.h
+++ calc-components/calculators/calculatordiff_impl.h
@@ -7,10 +7,12 @@ template <class T>
 CalculatorDiff<T>::CalculatorDiff(CALC_PTR(T) minuent,
                                   CALC_PTR(T) subtrahent,
                                   BUFFER_PTR(T) output,
-                                  AbstractAccessStrategy::Ptr accessStrategy) :
+                                  AbstractAccessStrategy::Ptr accessStrategy,
+                                  bool scalarSubtrahend) :
     CalculatorBase<T>(std::vector<CalcInterface::Ptr> {minuent, subtrahent}, output, accessStrategy),
     m_minuentBuffer(minuent->getOutputBuffer()),
-    m_subtrahentBuffer(subtrahent->getOutputBuffer())
+    m_subtrahentBuffer(subtrahent->getOutputBuffer()),
+    m_scalarSubtrahend(scalarSubtrahend)
 {
 }
 
@@ -18,11 +20,21 @@ template <class T>
 void CalculatorDiff<T>::doCalc(int sampleCount)
 {
     BUFFER_PTR(T) outBuff = CalculatorBase<T>::getOutputBuffer();
-    for(int i=0, currSample = CalculatorBase<T>::getSampleOffset();
-        i<sampleCount;
-        ++i, ++currSample) {
-        outBuff->at(currSample) =
-                m_minuentBuffer->at(currSample) - m_subtrahentBuffer->at(currSample);
+    if(!m_scalarSubtrahend) {
+        for(int i=0, currSample = CalculatorBase<T>::getSampleOffset();
+            i<sampleCount;
+            ++i, ++currSample) {
+            outBuff->at(currSample) =
+                    m_minuentBuffer->at(currSample) - m_subtrahentBuffer->at(currSample);
+        }
+    }
+    else {
+        for(int i=0, currSample = CalculatorBase<T>::getSampleOffset();
+            i<sampleCount;
+            ++i, ++currSample) {
+            outBuff->at(currSample) =
+                    m_minuentBuffer->at(currSample) - m_subtrahentBuffer->at(0);
+        }
     }
 }
 
