b317b248712e65d1ef450fa265eb7e37ae937950
Wilfried Schunk <w.schunk@zera.de>
2022-03-09 15:20:33 +0100

    unittest: add FFT_CONVERSION_AND_INVERS
    Signed-off-by: Wilfried Schunk <w.schunk@zera.de>

--- tests/unittest-calculatorfftwfloat.cpp
+++ tests/unittest-calculatorfftwfloat.cpp
@@ -2,6 +2,7 @@
 #include "buffers/buffertemplate.h"
 #include "calc-components/calculators/calculatornull.h"
 #include "calc-components/calculators/calculatorfftwfloat.h"
+#include "calc-components/calculators/calculatormult.h"
 #include "access-strategies/singlethreadedaccessstrategy.h"
 #include <gtest/gtest.h>
 #include <math.h>
@@ -43,9 +44,31 @@ TEST(TEST_FFT_FLOAT, FFT_CONVERSION) {
     calcTestCurve(buffTimeVals);
 
     CALC_PTR_FFTW(float) fftCalc = CalculatorFftwFloat::createWithOutBuffer<SingleThreadedAccessStrategy>(calcFFtIn, buffSize*2, false);
-    fftCalc->tryStartCalc(0);
+    fftCalc->tryStartCalc(buffSize);
 
-    BUFFER_PTR(float) fftBuff = fftCalc->getOutputBuffer();
     EXPECT_FLOAT_EQ(fftCalc->getFftRe(1), ampOrder1 * float(buffSize) / 2);
     EXPECT_FLOAT_EQ(fftCalc->getFftRe(2), ampOrder2 * float(buffSize) / 2);
+}
+
+TEST(TEST_FFT_FLOAT, FFT_CONVERSION_AND_INVERS) {
+    constexpr int buffSize = 256;
+
+    CALC_PTR(float) calcFFtIn = CalculatorNull<float>::createWithOutBuffer<SingleThreadedAccessStrategy>(buffSize);
+    BUFFER_PTR(float) buffTimeVals = calcFFtIn->getOutputBuffer();
+    calcTestCurve(buffTimeVals);
+
+    CALC_PTR_FFTW(float) fftCalc = CalculatorFftwFloat::createWithOutBuffer<SingleThreadedAccessStrategy>(calcFFtIn, buffSize*2, false);
+
+    CALC_PTR(float) calcNormal = CalculatorNull<float>::createWithOutBuffer<SingleThreadedAccessStrategy>(1);
+    calcNormal->getOutputBuffer()->at(0) = 1/float(buffSize);
+
+    CALC_PTR(float) fftCalcNormalised = CalculatorMult<float>::createWithOutBuffer<SingleThreadedAccessStrategy>(fftCalc, calcNormal, buffSize*2, true);
+
+    CALC_PTR_FFTW(float) fftCalcInvers = CalculatorFftwFloat::createWithOutBuffer<SingleThreadedAccessStrategy>(fftCalcNormalised, buffSize, true);
+    fftCalcInvers->tryStartCalc(buffSize);
+
+    BUFFER_PTR(float) curveBuff = fftCalcInvers->getOutputBuffer();
+    for (int i(0); i<buffSize; i++) {
+        EXPECT_NEAR(curveBuff->at(i), buffTimeVals->at(i),1e-4);
+    }
 }
